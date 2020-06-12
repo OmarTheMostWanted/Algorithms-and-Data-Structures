3169bf04958487cbe02c1768ccfb3614620793a5
themostwanted <omarthabettmw@gmail.com>
2020-06-12 21:15:58 +0200

    Binary Tree implementation

--- src/main/trees/Tree.java
+++ src/main/trees/Tree.java
@@ -39,41 +39,21 @@ public interface Tree<E> extends Iterable<E> {
             return 1 + depth(parent(p));
     }
 
-    default int height(Position<E> p) {
-        int h = 0; // base case (p is leaf)
-        for (Position<E> c : children(p))
-            h = Math.max(h, 1 + height(c));
-        return h;
-    }
+    int height(Position<E> p);
 
     /**
      * Node is visited before its children.
      **/
-    default void preorder(Position<E> p) {
-        visit(p); // visit performs some operation at the node, e.g. print its element
-        for (Position<E> c : children(p))
-            preorder(c);
-    }
+    void preorder(Position<E> p);
 
     /**
      * Node is visited after its children.
      **/
-    default void postorder(Position<E> p) {
-        for (Position<E> c : children(p))
-            postorder(c);
-        visit(p);
-    }
+    void postorder(Position<E> p);
 
 
-    /**Visits nodes per level (depth).**/
-//    default void breadthfirst(Position<E> p) {
-//        Queue<Position<E>> q = new Queue();
-//        q.enqueue(p);
-//        while (!q.isEmpty()) {
-//            Position<E> p = q.dequeue();
-//            visit(p);
-//            for (Position<E> c : children(p))
-//                q.enqueue(c);
-//        }
-//    }
+    /**
+     * Visits nodes per level (depth).
+     **/
+    void breadthfirst(Position<E> p);
 }
